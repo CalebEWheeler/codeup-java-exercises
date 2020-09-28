d2316abb5fda76a18a586b064629c4d935d1ccf3
Caleb Wheeler <caleb.edward.wheeler@gmail.com>
2020-09-28 15:38:42 -0500

    Feat: will complete the exercises for Syntax, Types, and Variables

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,6 +1,47 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.print("Hello, World! Hello, Hello. ");
-        System.out.print("Hello, World!");
+//        System.out.print("Hello, World! Hello, Hello. ");
+//        System.out.print("Hello, World!");
+
+//        int myFavoriteNumber = 27;
+//        System.out.print(myFavoriteNumber);
+
+//        double myString = 3.14;
+//        System.out.print(myString);
+
+//        int x = 5;
+//        System.out.println(x++);
+//        System.out.println(x);
+
+//        int x = 5;
+//        System.out.println(++x);
+//        System.out.println(x);
+
+//        String theNumberThree = "three";
+//        Object o = theNumberThree;
+//        int three = (int) o;
+
+//        int three = (int) "three";
+
+//        int x = 4;
+//        x += 5;
+//        System.out.print(x);
+
+//        int x = 3;
+//        int y = 4;
+//        y *= x;
+//        System.out.print(y);
+
+//        int x = 10;
+//        int y = 2;
+//        x /= y;
+//        y -= x;
+//        System.out.println(x);
+//        System.out.print(y);
+
+//        int max = 127;
+//        max++;
+//        System.out.println(max);
+
     }
 }
