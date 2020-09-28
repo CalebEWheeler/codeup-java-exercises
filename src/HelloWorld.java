43e32e7f7294278552617b8799e46a965e1a9163
Caleb Wheeler <caleb.edward.wheeler@gmail.com>
2020-09-28 13:54:20 -0500

    Feat: added another line to HelloWorld.java to see how text will print

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,6 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, World! Hello, Hello");
+        System.out.print("Hello, World! Hello, Hello. ");
+        System.out.print("Hello, World!");
     }
 }
