d44f225b4b642b57863f62fc08439b7e068620b7
Caleb Wheeler <caleb.edward.wheeler@gmail.com>
2020-10-01 08:46:55 -0500

    Feat: display second set of completed exercises for Java Strings in the curriculum

--- src/Bob.java
+++ src/Bob.java
@@ -1,2 +1,17 @@
-package PACKAGE_NAME;public class Bob {
+import java.util.Scanner;
+
+public class Bob {
+    public static void main(String [] args) {
+        System.out.println("What would you like to say to Bob?");
+        Scanner sc = new Scanner(System.in);
+        String userInput = sc.nextLine();
+        String respondQuestion = "Sure.";
+        String respondExclaimation = "Woah, chill out!";
+        String noResponse = "Fine be that way!";
+        String genericResponse = "Whatever.";
+        if (userInput.endsWith("?")) System.out.printf("%s", respondQuestion);
+        else if (userInput.endsWith("!")) System.out.printf("%s", respondExclaimation);
+        else if (userInput.equals("")) System.out.printf("%s", noResponse);
+        else System.out.printf("%s", genericResponse);
+    }
 }
