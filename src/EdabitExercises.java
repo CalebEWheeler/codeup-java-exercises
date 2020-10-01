05eda41bf3e83f3614114205d09d6158632ccdf0
Caleb Wheeler <caleb.edward.wheeler@gmail.com>
2020-10-01 10:14:17 -0500

    Feat: problems 1 and 2 after completion in edabit

--- src/EdabitExercises.java
+++ src/EdabitExercises.java
@@ -1,8 +1,23 @@
 public class EdabitExercises {
-    public static boolean returnTrue() {
-        return true;
+//ALL PROBLEMS SHOWN BELOW ARE PASTED TO THIS REPO FROM
+// EDABIT AFTER THEY HAVE BEEN TESTED AND SUBMITTED.
 
+
+    //1. All you have to do is type return true; between the curly braces { }
+    public static class Program {
+        public boolean returnTrue() {
+            return true;
+
+        }
     }
 
 
+    //2. Create a method that takes two integers as arguments and return their sum.
+    public class SumOfTwoNumbers {
+        public static int sum(int a, int b) {
+            return a + b;
+        }
+    }
+
+    //3.
 }
