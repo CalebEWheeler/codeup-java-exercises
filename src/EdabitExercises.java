c1df4beae048bf51351ee77dcf9db9ea9b2fc8b1
Caleb Wheeler <caleb.edward.wheeler@gmail.com>
2020-10-01 11:40:36 -0500

    Feat: display completed edabit problem 9

--- src/EdabitExercises.java
+++ src/EdabitExercises.java
@@ -69,7 +69,16 @@ public class EdabitExercises {
         }
     }
 
-    //9.
+    //9. There is a single operator in Java, capable of providing the remainder of a division
+    // operation. Two numbers are passed as parameters. The first parameter divided by the
+    // second parameter will have a remainder, possibly zero. Return that value.
+    public class Challenge {
+        public static int remainder(int a, int b) {
+            int findRemainder = (a%b);
+            return findRemainder;
+        }
+    }
 
+    //10.
 
 }
