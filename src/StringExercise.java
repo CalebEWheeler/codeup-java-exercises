d8078a11ab23490170ab36bb96a375d9cf1f64ec
Caleb Wheeler <caleb.edward.wheeler@gmail.com>
2020-10-01 08:45:53 -0500

    Feat: display first set of completed exercises for Java Strings in the curriculum

---
+++ src/StringExercise.java
@@ -0,0 +1,15 @@
+public class StringExercise {
+    public static void main(String [] args) {
+        String line1 = "We don't need no education";
+        String line2 = "We don't need no thought control";
+        System.out.println(line1 + "\n" + line2);
+        String line3 = "Check \"this\" out!, \"s inside of \"s!";
+        System.out.println(line3);
+        String line4 = "In windows, the main drive is usually C:\\ ";
+        System.out.println(line4);
+        String line5 = "I can do backslashes \\, double backslashes \\\\,\nand the amazing triple backslash \\\\\\!";
+        System.out.println(line5);
+
+
+    }
+}
