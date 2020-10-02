48156c34abda68f817280899365060b5f0dba662
Caleb Wheeler <caleb.edward.wheeler@gmail.com>
2020-10-02 12:47:07 -0500

    feat: add a randomizer method and refactored main to prompt the user to guess a number between 0-100. Currently working to test if the user's guess is higher/lower/or correct and displaying proper messages.

--- src/HighLow.java
+++ src/HighLow.java
@@ -2,15 +2,26 @@ import java.util.Scanner;
 
 public class HighLow {
     public static void main(String [] args) {
-        System.out.println("Please enter a number between 1 - 100: ");
+        int random = randomizer(0, 100);
+        System.out.println("Guess a number between 0-100:");
         Scanner sc = new Scanner(System.in);
         int enteredNum = Integer.parseInt(sc.nextLine());
         if(enteredNum < 0 || enteredNum > 100) {
-            System.out.println("That's not a valid number!");
+            System.out.println("That's not a valid guess!");
         }
-        else System.out.printf("You entered: %d", enteredNum);
-
-
+        else {
+            if (enteredNum < random) System.out.println("HIGHER");
+            else if (enteredNum > random) System.out.println("LOWER");
+            else System.out.println("GOOD GUESS");
+        }
     }
 
+    public static int randomizer(int min, int max) {
+        int range = (max-min) + 1;
+        return (int) (Math.random() * range) + min;
+    }
+
+//    public static String guessGame() {
+//
+//    }
 }
