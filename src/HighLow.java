58b9e11ada9f5d0db71fca23992a59242963a73e
Caleb Wheeler <caleb.edward.wheeler@gmail.com>
2020-10-02 12:51:14 -0500

    feat: pulled main() content out and placed it into new guessGame().

--- src/HighLow.java
+++ src/HighLow.java
@@ -2,18 +2,7 @@ import java.util.Scanner;
 
 public class HighLow {
     public static void main(String [] args) {
-        int random = randomizer(0, 100);
-        System.out.println("Guess a number between 0-100:");
-        Scanner sc = new Scanner(System.in);
-        int enteredNum = Integer.parseInt(sc.nextLine());
-        if(enteredNum < 0 || enteredNum > 100) {
-            System.out.println("That's not a valid guess!");
-        }
-        else {
-            if (enteredNum < random) System.out.println("HIGHER");
-            else if (enteredNum > random) System.out.println("LOWER");
-            else System.out.println("GOOD GUESS");
-        }
+        guessGame();
     }
 
     public static int randomizer(int min, int max) {
@@ -21,7 +10,20 @@ public class HighLow {
         return (int) (Math.random() * range) + min;
     }
 
-//    public static String guessGame() {
-//
-//    }
+    public static String guessGame() {
+        int random = randomizer(0, 100);
+        System.out.println("Guess a number between 0-100:");
+        Scanner sc = new Scanner(System.in);
+        int enteredNum = Integer.parseInt(sc.nextLine());
+        if(enteredNum < 0 || enteredNum > 100) {
+            return "That's not a valid guess!";
+        }
+        else {
+            if (enteredNum < random) return "HIGHER";
+            else if (enteredNum > random) return "LOWER";
+            else return "GOOD GUESS";
+        }
+    }
+
+
 }
