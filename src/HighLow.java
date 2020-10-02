6b3cbedb6dfb4f089fdf0151bf2b97628740afe4
Caleb Wheeler <caleb.edward.wheeler@gmail.com>
2020-10-02 12:13:55 -0500

    feat: will now filter the user's input and display a message if the entered number is not between 0-100. If the number is between 0-100 it will display the entered number.

--- src/HighLow.java
+++ src/HighLow.java
@@ -2,9 +2,15 @@ import java.util.Scanner;
 
 public class HighLow {
     public static void main(String [] args) {
-        System.out.println("Please enter a number: ");
+        System.out.println("Please enter a number between 1 - 100: ");
         Scanner sc = new Scanner(System.in);
-        String enteredNum = sc.nextLine();
-        System.out.printf("You entered: %s", enteredNum);
+        int enteredNum = Integer.parseInt(sc.nextLine());
+        if(enteredNum < 0 || enteredNum > 100) {
+            System.out.println("That's not a valid number!");
+        }
+        else System.out.printf("You entered: %d", enteredNum);
+
+
     }
+
 }
