24eb157a5086a143ed69d1e5ead2a51076a3599b
Caleb Wheeler <caleb.edward.wheeler@gmail.com>
2020-10-02 16:08:10 -0500

    feat: add a for loop to limit the number of tries a user has when guessing. Runs as I like it to besides when a user wins and they don't wish to continue the output is correct, but an error runs afterwards.

--- src/HighLow.java
+++ src/HighLow.java
@@ -1,29 +1,53 @@
 import java.util.Scanner;
 
 public class HighLow {
-    public static void main(String [] args) {
+    public static void main(String[] args) {
         guessGame();
     }
 
+    public static void guessGame() {
+        int random = randomizer(0, 100);
+        int tries = 6;
+        System.out.println("Guess a number between 0-100: ");
+        Scanner sc = new Scanner(System.in);
+        for (int i = 0; i < tries; i++) {
+        int enteredNum = Integer.parseInt(sc.nextLine());
+        if (enteredNum < 0 || enteredNum > 100) {
+            System.out.println("That's not a valid guess!\nGuess again! I'm counting that one. ");
+        } else {
+                if (enteredNum < random && tries - 1 > i) System.out.println("HIGHER: Guess again!");
+                else if (enteredNum > random && tries - 1 > i) System.out.println("LOWER: Guess again!");
+                else if (i == tries - 1) {
+                    System.out.println("You're all out of tries!\nWould you like to play again? [yes/no]");
+                    String playAgain = sc.next();
+                    boolean yes = playAgain.equals("yes");
+                    boolean no = playAgain.equals("no");
+                    if (yes) {
+                        guessGame();
+                    }
+                    else if (no) System.out.println("Bummer, I was having fun winning..");
+                }
+                else if (enteredNum == random) {
+                    System.out.println("GOOD GUESS, YOU WIN!\nWould you like to play again? [yes/no]");
+                    String playAgain = sc.next();
+                    boolean yes = playAgain.equals("yes");
+                    boolean no = playAgain.equals("no");
+                    if (yes) guessGame();
+                    else if (no) System.out.println("Bummer, I wanted to settle the score..");
+                }
+            }
+        }
+    }
+
     public static int randomizer(int min, int max) {
-        int range = (max-min) + 1;
+        int range = (max - min) + 1;
         return (int) (Math.random() * range) + min;
     }
-
-    public static String guessGame() {
-        int random = randomizer(0, 100);
-        System.out.println("Guess a number between 0-100:");
-        Scanner sc = new Scanner(System.in);
-        int enteredNum = Integer.parseInt(sc.nextLine());
-        if(enteredNum < 0 || enteredNum > 100) {
-            return "That's not a valid guess!";
-        }
-        else {
-            if (enteredNum < random) return "HIGHER";
-            else if (enteredNum > random) return "LOWER";
-            else return "GOOD GUESS";
-        }
-    }
-
-
 }
+
+//    Exception in thread "main" java.lang.NumberFormatException: For input string: ""
+//        at java.base/java.lang.NumberFormatException.forInputString(NumberFormatException.java:65)
+//        at java.base/java.lang.Integer.parseInt(Integer.java:662)
+//        at java.base/java.lang.Integer.parseInt(Integer.java:770)
+//        at HighLow.guessGame(HighLow.java:14)
+//        at HighLow.main(HighLow.java:5)
