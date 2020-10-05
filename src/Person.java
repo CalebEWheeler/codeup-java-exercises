a7682c3e68746c03e9013b7d639d84de0a0fd271
Caleb Wheeler <caleb.edward.wheeler@gmail.com>
2020-10-05 17:02:32 -0500

    feat: completed the first part of the exercise for Object Oriented Programming

--- src/Person.java
+++ src/Person.java
@@ -1,15 +1,24 @@
 public class Person {
-    private static String name;
-    public String getName(){
-//TODO: return the person's name
-        return Person.name;
-    }
 
-    public void setName(String name){
+        private String name;
+        public String getName () {
+//TODO: return the person's name
+            return this.name;
+        }
+
+        public void setName (String name){
 //TODO: change the name property to the passed value
-//        return
-    }
-    public void sayHello(){
+            this.name = name;
+        }
+        public void sayHello () {
 //TODO: print a message to the console using the person's name
+            System.out.printf("Hello, my name is %s!", this.getName());
+        }
+
+    public static void main(String[] args) {
+        Person p1 = new Person();
+        p1.setName("Caleb");
+//        System.out.println(p1.name);
+        p1.sayHello();
     }
 }
