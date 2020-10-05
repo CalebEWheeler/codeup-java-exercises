4c43d36e76cb70bec584a13b9bd899c919b6e574
Caleb Wheeler <caleb.edward.wheeler@gmail.com>
2020-10-05 13:49:51 -0500

    feat: started the exercise for the day, created Person.class

---
+++ src/Person.java
@@ -0,0 +1,15 @@
+public class Person {
+    private static String name;
+    public String getName(){
+//TODO: return the person's name
+        return Person.name;
+    }
+
+    public void setName(String name){
+//TODO: change the name property to the passed value
+//        return
+    }
+    public void sayHello(){
+//TODO: print a message to the console using the person's name
+    }
+}
