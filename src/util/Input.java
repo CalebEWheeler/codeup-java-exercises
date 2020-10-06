c3e955b5b1a352a798660438f948870c787020b9
Caleb Wheeler <caleb.edward.wheeler@gmail.com>
2020-10-06 10:11:06 -0500

    feat: imported and added private scanner to Input class, and added the required methods to the Input class

--- src/util/Input.java
+++ src/util/Input.java
@@ -1,4 +1,27 @@
 package util;
-
+import java.util.Scanner;
 public class Input {
+    private Scanner scanner = new Scanner(System.in);
+    String getString() {
+
+    }
+//    boolean yesNo() {
+//
+//    }
+//    int getInt(int min, int max) {
+//
+//    }
+//    double getDouble(double min, double max) {
+//
+//    }
+//    double getDouble() {
+//
+//    }
+
+    public static void main(String[] args) {
+        Input userInput = new Input();
+        System.out.println(userInput.getString());
+
+    }
+
 }
