1643b65bb6acb02c35a8eae2d0121f32bfd687a9
Caleb Wheeler <caleb.edward.wheeler@gmail.com>
2020-10-06 14:53:28 -0500

    feat: refactored Input.class & InputTest.class to show Daniel's methods

--- src/util/Input.java
+++ src/util/Input.java
@@ -15,50 +15,66 @@ public class Input {
     }
         String getString () {
             System.out.println("Enter a string: ");
-            return scanner.nextLine();
+            return this.scanner.nextLine();
         }
-        boolean yesNo () {
-            System.out.println("Enter: y/yes/Yes/YES or enter no");
-            String input = scanner.nextLine();
-            if (input.equals("y") || input.equalsIgnoreCase("yes") || input.startsWith("Y")) return true;
-            else return false;
-        }
-        int getInt ( int min, int max){
-            System.out.println("Please enter a number between 1-10");
-            int input = scanner.nextInt();
-            if (input >= min && input <= max) return input;
-            else return getInt(1, 10);
-        }
-        double getDouble ( double min, double max){
-            System.out.println("Please enter a number between 5.7-15.7");
-            double input = scanner.nextDouble();
-            if (input >= min && input <= max) return input;
-            else return getDouble(5.7, 15.7);
-        }
-        double getDouble () {
-            System.out.println("Please enter two numbers: ");
-            double input1 = scanner.nextDouble();
-            double input2 = scanner.nextDouble();
-            System.out.println("Please enter a number that is between the " +
-                    "two numbers you entered earlier.");
-            double input3 = scanner.nextDouble();
-            if (input3 >= input1 && input3 <= input2) return input3;
-            else return getDouble();
+        public boolean yesNo () {
+            System.out.println("Enter: yes or no");
+            String input = this.scanner.nextLine();
+//            if (input.equals("y") || input.equalsIgnoreCase("yes") || input.startsWith("Y")) return true;
+//            else return false;
 
+            //Daniel's method
+            return (input.trim().toLowerCase().startsWith("y") ||
+                    input.trim().toLowerCase().startsWith("yes"));
+        }
+        public int getInt ( int min, int max){
+//            System.out.println("Please enter a number between 1-10");
+//            int input = scanner.nextInt();
+//            if (input >= min && input <= max) return input;
+//            else return getInt(1, 10);
+
+            //Daniel's method
+            while (true) {
+                System.out.printf("Please enter an integer between %d and %d.\n", min, max);
+
+                String input = scanner.nextLine();
+                int num = Integer.parseInt(input);
+                if (num >= min && num <= max)
+                    return num;
+            }
+        }
+        public int getInt() {
+            String input = scanner.nextLine();
+            int num = Integer.parseInt(input);
+            return num;
+        }
+        public double getDouble ( double min, double max){
+//            System.out.println("Please enter a number between 5.7-15.7");
+//            double input = scanner.nextDouble();
+//            if (input >= min && input <= max) return input;
+//            else return getDouble(5.7, 15.7);
+
+            //Daniel's method
+            while (true) {
+                System.out.printf("Please enter a double between %.2f and %.2f.\n", min, max);
+
+                String input = scanner.nextLine();
+                double num = Double.parseDouble(input);
+                if (num >= min && num <= max)
+                    return num;
+            }
+        }
+        public double getDouble () {
+            String input = scanner.nextLine();
+            return Double.parseDouble(input);
         }
 
         //Circle.class portion
-//        public static void getInfo () {
-            //will get the radius if only getRadius() is active
-//            Circle c1 = new Circle();
-//            System.out.printf("This is the radius you entered: %d", c1.getRadius());
-
-            //refactor Circle() to return the radius
-//        Circle c1 = new Circle(radius);
-//        System.out.println();
+        public static void getInfo () {
 
 
 
-//        }
+
+        }
 
 }
--- src/util/InputTest.java
+++ src/util/InputTest.java
@@ -3,10 +3,14 @@ package util;
 public class InputTest {
     public static void main(String[] args) {
         Input userInput = new Input();
-        System.out.println(userInput.getString());
-        System.out.println(userInput.yesNo());
-        System.out.println(userInput.getInt(1, 10) + ": is a valid input!");
-        System.out.println(userInput.getDouble(5.7, 15.7) + ": is a valid input!");
+//        System.out.println(userInput.getString());
+//        if (userInput.yesNo())
+//            System.out.println("This returned a true");
+//        else
+//            System.out.println("It actually said no");
+        System.out.println(userInput.getInt(10, 20));
+        System.out.println(userInput.getInt());
+        System.out.println(userInput.getDouble(5.7, 15.7));
         System.out.println(userInput.getDouble() + ": is a valid input!");
     }
 
