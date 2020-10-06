572e26b450da79786d19ccc1f1986d31c5652879
Caleb Wheeler <caleb.edward.wheeler@gmail.com>
2020-10-06 09:43:45 -0500

    feat: complete Person.java exercise

--- src/Person.java
+++ src/Person.java
@@ -1,6 +1,10 @@
 public class Person {
 
+
         private String name;
+    public Person(String personName) {
+        this.name = personName;
+    }
         public String getName () {
 //TODO: return the person's name
             return this.name;
@@ -16,9 +20,22 @@ public class Person {
         }
 
     public static void main(String[] args) {
-        Person p1 = new Person();
-        p1.setName("Caleb");
-//        System.out.println(p1.name);
-        p1.sayHello();
+//        Person person1 = new Person("John");
+//        Person person2 = new Person("John");
+//        System.out.println(person1.getName().equals(person2.getName()));
+//        System.out.println(person1 == person2);
+
+//        Person person1 = new Person("John");
+//        Person person2 = person1;
+//        System.out.println(person1 == person2);
+
+        Person person1 = new Person("John");
+        Person person2 = person1;
+        System.out.println(person1.getName());
+        System.out.println(person2.getName());
+        person2.setName("Jane");
+        System.out.println(person1.getName());
+        System.out.println(person2.getName());
+
     }
 }
