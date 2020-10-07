e9308ca20befea3037ad843a4b7d0a60c9044f69
Caleb Wheeler <caleb.edward.wheeler@gmail.com>
2020-10-07 12:10:26 -0500

    feat: complete the second part of the exercise for Java Arrays

---
+++ src/ServerNameGenerator.java
@@ -0,0 +1,23 @@
+public class ServerNameGenerator {
+
+
+    public static String randomAdjective() {
+        String[] adjectives = {"windy", "sticky", "abiding", "adaptable", "deeply", "long", "temporary", "forgetful", "well-groomed", "imminent"};
+        double length = adjectives.length;
+        double rand = Math.floor(Math.random() * length);
+        int random = (int) rand;
+        return adjectives[random];
+    }
+    public static String randomNoun() {
+        String[] nouns = {"dog", "cat", "car", "bug", "bear", "key", "pen", "cup", "plate", "chair"};
+        double length = nouns.length;
+        double rand = Math.floor(Math.random() * length);
+        int random = (int) rand;
+        return nouns[random];
+    }
+
+    public static void main(String[] args) {
+        System.out.println(randomAdjective() + "-" + randomNoun());
+
+    }
+}
