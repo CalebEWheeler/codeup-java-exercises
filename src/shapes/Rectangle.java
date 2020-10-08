bfef34219aa3d52da91d6d979d6152ae1b9a2f35
Caleb Wheeler <caleb.edward.wheeler@gmail.com>
2020-10-08 15:06:43 -0500

    feat: create Rectangle.class in shapes dir. Will display protected length and width properties as well as a basic constructor to begin the exercise for Inheritence and Polymorphism

---
+++ src/shapes/Rectangle.java
@@ -0,0 +1,11 @@
+package shapes;
+
+public class Rectangle {
+    protected int length;
+    protected int width;
+
+    public Rectangle(int rectangleLength, int rectangleWidth) {
+        this.length = rectangleLength;
+        this.width = rectangleWidth;
+    }
+}
