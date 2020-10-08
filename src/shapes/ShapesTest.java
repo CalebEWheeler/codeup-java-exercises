3e76a64b878812cabfb2c85894489f70638a782a
Caleb Wheeler <caleb.edward.wheeler@gmail.com>
2020-10-08 16:29:21 -0500

    feat: display a new box2 with a side value of 5, followed by a message that prints the perimeter and area of box2

--- src/shapes/ShapesTest.java
+++ src/shapes/ShapesTest.java
@@ -5,5 +5,9 @@ public class ShapesTest {
         Rectangle box1 = new Rectangle(4,5);
         System.out.println("The Perimeter of box1: " + box1.getPerimeter());
         System.out.println("The Area of box1: " + box1.getArea());
+
+        Rectangle box2 = new Square(5);
+        System.out.println("The Perimeter of box2: " + box2.getPerimeter());
+        System.out.println("The Area of box2: " + box2.getArea());
     }
 }
--- src/shapes/Square.java
+++ src/shapes/Square.java
@@ -5,4 +5,6 @@ public class Square extends Rectangle {
     public Square(int side) {
         super(side, side);
     }
+
+
 }
