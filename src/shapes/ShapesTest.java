0941e435adfdcfb036f4a4536222a5aaa407755e
Caleb Wheeler <caleb.edward.wheeler@gmail.com>
2020-10-08 17:42:08 -0500

    feat: display a box2 that accepts the Square.class getArea() and getPerimeter() by overriding. Display box3 that takes all methods from Square.class

--- src/shapes/ShapesTest.java
+++ src/shapes/ShapesTest.java
@@ -2,12 +2,20 @@ package shapes;
 
 public class ShapesTest {
     public static void main(String[] args) {
+        //New box taking everything from Rectangle.class
         Rectangle box1 = new Rectangle(4,5);
         System.out.println("The Perimeter of box1: " + box1.getPerimeter());
         System.out.println("The Area of box1: " + box1.getArea());
 
+        //Overriding methods example
         Rectangle box2 = new Square(5);
         System.out.println("The Perimeter of box2: " + box2.getPerimeter());
         System.out.println("The Area of box2: " + box2.getArea());
+
+        //New box taking everything from Square.class
+        Square box3 = new Square(6);
+        System.out.println("The Perimeter of box3: " + box3.getPerimeter());
+        System.out.println("The Area of box3: " + box3.getArea());
+
     }
 }
--- src/shapes/Square.java
+++ src/shapes/Square.java
@@ -1,10 +1,19 @@
 package shapes;
 
 public class Square extends Rectangle {
+    protected int side;
     //Square constructor
     public Square(int side) {
         super(side, side);
+        this.side = side;
     }
 
-
+    public int getPerimeter() {
+        System.out.println("\nusing squares method!");
+        return (4 * this.side);
+    }
+    public int getArea() {
+        System.out.println("\nusing squares method!");
+        return (int) Math.pow(this.side, 2);
+    }
 }
