fb8525c5194241496211ed888eec259bc4bef472
Caleb Wheeler <caleb.edward.wheeler@gmail.com>
2020-10-08 16:26:25 -0500

    feat: create ShapesTest.class, created box1 and printed out the perimeter and area of box1

---
+++ src/shapes/ShapesTest.java
@@ -0,0 +1,9 @@
+package shapes;
+
+public class ShapesTest {
+    public static void main(String[] args) {
+        Rectangle box1 = new Rectangle(4,5);
+        System.out.println("The Perimeter of box1: " + box1.getPerimeter());
+        System.out.println("The Area of box1: " + box1.getArea());
+    }
+}
