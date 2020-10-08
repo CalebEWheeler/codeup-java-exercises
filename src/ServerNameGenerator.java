6d0b2e25110c11cd8ee94dcfa5c211770a880cae
Caleb Wheeler <caleb.edward.wheeler@gmail.com>
2020-10-08 09:43:15 -0500

    feat: added Fer's solution to randomize the word and added a sout that displays his solution

--- src/ServerNameGenerator.java
+++ src/ServerNameGenerator.java
@@ -1,15 +1,15 @@
+import java.util.Random;
 public class ServerNameGenerator {
-
+    public static String[] adjectives = {"windy", "sticky", "abiding", "adaptable", "deeply", "long", "temporary", "forgetful", "well-groomed", "imminent"};
+    public static String[] nouns = {"dog", "cat", "car", "bug", "bear", "key", "pen", "cup", "plate", "chair"};
 
     public static String randomAdjective() {
-        String[] adjectives = {"windy", "sticky", "abiding", "adaptable", "deeply", "long", "temporary", "forgetful", "well-groomed", "imminent"};
         double length = adjectives.length;
         double rand = Math.floor(Math.random() * length);
         int random = (int) rand;
         return adjectives[random];
     }
     public static String randomNoun() {
-        String[] nouns = {"dog", "cat", "car", "bug", "bear", "key", "pen", "cup", "plate", "chair"};
         double length = nouns.length;
         double rand = Math.floor(Math.random() * length);
         int random = (int) rand;
@@ -18,9 +18,13 @@ public class ServerNameGenerator {
 
     public static void main(String[] args) {
         System.out.println("Here is your server name:\n" + randomAdjective() + "-" + randomNoun());
-
+        System.out.println("Here is your server name from Fer:\n" + getWord(adjectives) + "-" + getWord(nouns));
     }
 
-    //Fer's walkthrough
-
+    //Fer's randomizer
+    public static String getWord(String[] words) {
+        Random rnd = new Random();
+        int randomIndex = rnd.nextInt(words.length);
+        return words[randomIndex];
+    }
 }
