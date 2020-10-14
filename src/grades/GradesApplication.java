7abc0717a1f48ffbe604b0b806806c44b5b7a6cc
Caleb Wheeler <caleb.edward.wheeler@gmail.com>
2020-10-14 10:39:54 -0500

    feat: display complete part 3 for collections exercise, will need to come back to finish the bonuses to fully grasp ArrayList and HashMap

--- src/grades/GradesApplication.java
+++ src/grades/GradesApplication.java
@@ -1,5 +1,8 @@
 package grades;
+
 import java.util.HashMap;
+
+import util.Input;
 
 public class GradesApplication {
 
@@ -10,23 +13,52 @@ public class GradesApplication {
         Student cruzanio = new Student("Cruzanio");
         Student cory = new Student("Cory");
 
-        caleb.addGrade(89); caleb.addGrade(82); caleb.addGrade(92);
-        sahara.addGrade(94); sahara.addGrade(81); sahara.addGrade(86);
-        cruzanio.addGrade(96); cruzanio.addGrade(93); cruzanio.addGrade(90);
-        cory.addGrade(88); cory.addGrade(99); cory.addGrade(93);
+        caleb.addGrade(89);
+        caleb.addGrade(82);
+        caleb.addGrade(92);
+        sahara.addGrade(94);
+        sahara.addGrade(81);
+        sahara.addGrade(86);
+        cruzanio.addGrade(96);
+        cruzanio.addGrade(93);
+        cruzanio.addGrade(90);
+        cory.addGrade(88);
+        cory.addGrade(99);
+        cory.addGrade(93);
 
         students.put("CalebEWheeler", caleb);
         students.put("saharatijol", sahara);
         students.put("cruzaniovillarreal", cruzanio);
         students.put("coryholley", cory);
 
-        System.out.println(caleb.getName());
-
-//        System.out.println("Welcome!");
-//        System.out.println("Here are the Github usernames of out students: ");
-//        for (String username : students.get(student1))
-//        System.out.printf("|%s|  ", username);
+        cli(students);
     }
-    
+
+    public static void cli(HashMap<String, Student> students) {
+        Input input = new Input();
+        System.out.println("Welcome!\n" +
+                "\nHere are the Github usernames of our students: \n");
+        for (String ghUsername : students.keySet()) {
+            System.out.print(ghUsername + " ");
+        }
+        do {
+
+            System.out.println("\nWhat student would you like to see more information on?");
+            String keyTyped = input.getString();
+
+            if (students.containsKey(keyTyped)) {
+                System.out.printf("Name: %s - Github Username: %s\n" +
+                                "Current Average: %f\n",
+                        students.get(keyTyped).getName(),
+                        keyTyped,
+                        students.get(keyTyped).getGradeAverage());
+            } else {
+                System.err.println("Sorry, no student found with the GitHub username of "
+                        + keyTyped);
+            }
+        } while (input.yesNo("Would you like to see another student? Y/N"));
+
+        System.out.println("Goodbye, and have a wonderful day!");
+    }
 
 }
--- src/util/Input.java
+++ src/util/Input.java
@@ -1,4 +1,5 @@
 package util;
+
 import java.util.Scanner;
 
 public class Input {
@@ -11,60 +12,69 @@ public class Input {
         //NEW SCANNER GENERATED
         this.scanner = new Scanner(System.in);
     }
-        String getString () {
-            System.out.println("Enter a string: ");
-            return this.scanner.nextLine();
-        }
-        public boolean yesNo () {
-            System.out.println("Enter: yes or no");
-            String input = this.scanner.nextLine();
-//            if (input.equals("y") || input.equalsIgnoreCase("yes") || input.startsWith("Y")) return true;
-//            else return false;
 
-            //Daniel's method
-            return (input.trim().toLowerCase().startsWith("y") ||
-                    input.trim().toLowerCase().startsWith("yes"));
-        }
-        public int getInt ( int min, int max){
+    public String getString() {
+        return this.scanner.nextLine();
+    }
+
+    public String getString(String prompt) {
+        System.out.println(prompt);
+        return getString();
+    }
+
+    public boolean yesNo() {
+        return yesNo("Please enter yes or no");
+    }
+
+    public boolean yesNo(String prompt) {
+        System.out.println(prompt);
+        String input = this.scanner.nextLine();
+        return (input.trim().toLowerCase().equals("y") ||
+                input.trim().toLowerCase().equals("yes"));
+    }
+    public int getInt(int min, int max) {
 //            System.out.println("Please enter a number between 1-10");
 //            int input = scanner.nextInt();
 //            if (input >= min && input <= max) return input;
 //            else return getInt(1, 10);
 
-            //Daniel's method
-            while (true) {
-                System.out.printf("Please enter an integer between %d and %d.\n", min, max);
+        //Daniel's method
+        while (true) {
+            System.out.printf("Please enter an integer between %d and %d.\n", min, max);
 
-                String input = scanner.nextLine();
-                int num = Integer.parseInt(input);
-                if (num >= min && num <= max)
-                    return num;
-            }
-        }
-        public int getInt() {
             String input = scanner.nextLine();
             int num = Integer.parseInt(input);
-            return num;
+            if (num >= min && num <= max)
+                return num;
         }
-        public double getDouble ( double min, double max){
+    }
+
+    public int getInt() {
+        String input = scanner.nextLine();
+        int num = Integer.parseInt(input);
+        return num;
+    }
+
+    public double getDouble(double min, double max) {
 //            System.out.println("Please enter a number between 5.7-15.7");
 //            double input = scanner.nextDouble();
 //            if (input >= min && input <= max) return input;
 //            else return getDouble(5.7, 15.7);
 
-            //Daniel's method
-            while (true) {
-                System.out.printf("Please enter a double between %.2f and %.2f.\n", min, max);
+        //Daniel's method
+        while (true) {
+            System.out.printf("Please enter a double between %.2f and %.2f.\n", min, max);
 
-                String input = scanner.nextLine();
-                double num = Double.parseDouble(input);
-                if (num >= min && num <= max)
-                    return num;
-            }
-        }
-        public double getDouble () {
             String input = scanner.nextLine();
-            return Double.parseDouble(input);
+            double num = Double.parseDouble(input);
+            if (num >= min && num <= max)
+                return num;
         }
+    }
+
+    public double getDouble() {
+        String input = scanner.nextLine();
+        return Double.parseDouble(input);
+    }
 
 }
