ee529e79e0b7464139b952de7edea5624dae2cb7
Caleb Wheeler <caleb.edward.wheeler@gmail.com>
2020-10-22 15:52:19 -0500

    feat: create a constructor that takes in two strings and a double, and sets type, brand, and price to those values

--- src/javaAssessment1/studyGuide/java_II_Device.java
+++ src/javaAssessment1/studyGuide/java_II_Device.java
@@ -5,4 +5,9 @@ public class java_II_Device {
     private String brand;
     private double price;
 
+    public java_II_Device(String type, String brand, double price) {
+        this.type = type;
+        this.brand = brand;
+        this.price = price;
+    }
 }
