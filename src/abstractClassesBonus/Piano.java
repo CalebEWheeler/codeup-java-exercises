0b0048369e751d519e7dd80df0ca1aa42c4d6196
Caleb Wheeler <caleb.edward.wheeler@gmail.com>
2020-10-23 16:36:57 -0500

    feat: define all methods implemented from Tunable and VolumeAdjustable interfaces in Piano.class and Sax.class

--- src/abstractClassesBonus/Piano.java
+++ src/abstractClassesBonus/Piano.java
@@ -4,4 +4,17 @@ public abstract class Piano extends MusicalInstrument implements Tuneable, Volum
     public Piano(String instrumentClassification) {
         super(instrumentClassification);
     }
+
+    public void tuneInstrument() {
+        System.out.println("Tuning instrument");
+    }
+    public void detuneInstrument() {
+        System.out.println("Detuning instrument");
+    }
+    public void volumeUp() {
+        System.out.println("Turning the volume up");
+    }
+    public void volumeDown() {
+        System.out.println("Turning the volume down");
+    }
 }
--- src/abstractClassesBonus/Sax.java
+++ src/abstractClassesBonus/Sax.java
@@ -4,4 +4,17 @@ public abstract class Sax extends MusicalInstrument implements Tuneable, VolumeA
     public Sax(String instrumentClassification) {
         super(instrumentClassification);
     }
+
+    public void tuneInstrument() {
+        System.out.println("Tuning instrument");
+    }
+    public void detuneInstrument() {
+        System.out.println("Detuning instrument");
+    }
+    public void volumeUp() {
+        System.out.println("Turning the volume up");
+    }
+    public void volumeDown() {
+        System.out.println("Turning the volume down");
+    }
 }
