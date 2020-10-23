7fa108d9ee0fda6b8ea274d4149d138d91ec706c
Caleb Wheeler <caleb.edward.wheeler@gmail.com>
2020-10-22 19:17:45 -0500

    feat: create getters and setters for the Device() properties

--- src/javaAssessment1/studyGuide/java_II_Device.java
+++ src/javaAssessment1/studyGuide/java_II_Device.java
@@ -5,9 +5,29 @@ public class java_II_Device {
     private String brand;
     private double price;
 
-    public java_II_Device(String type, String brand, double price) {
+    public java_II_Device(String type, String brand, double price) throws IllegalArgumentException{
         this.type = type;
         this.brand = brand;
         this.price = price;
+        try {
+            if (type == null || brand == null) {throw new IllegalArgumentException(); }
+            if (price == 0) {throw new IllegalArgumentException();}
+
+        } catch(IllegalArgumentException iae) {
+            System.err.println(iae);
+        }
+
+    }
+
+    public String getType() {return type;}
+    public String getBrand() {return brand;}
+    public double getPrice() {return price;}
+
+    public void setType(String type) {this.type = type;}
+    public void setBrand(String brand) {this.brand = brand;}
+    public void setPrice(double price) {this.price = price;}
+
+    public static void main(String[] args) {
+        java_II_Device phone = new java_II_Device("galaxy", "samsung", 0);
     }
 }
