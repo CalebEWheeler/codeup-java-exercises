353b7df4e110eec01541dbf752375d9a00850dfd
Caleb Wheeler <caleb.edward.wheeler@gmail.com>
2020-10-23 16:22:09 -0500

    feat: create constructor for MusicalInstrument() that sets the value of instrumentClassification and also create getter and setter methods

--- src/abstractClassesBonus/MusicalInstrument.java
+++ src/abstractClassesBonus/MusicalInstrument.java
@@ -1,5 +1,14 @@
 package abstractClassesBonus;
 
 abstract class MusicalInstrument implements Playable{
+    private String instrumentClassification;
+    public MusicalInstrument(String instrumentClassification) {
+        this.instrumentClassification = instrumentClassification;
+    }
+
+    public String getInstrumentClassification() {return this.instrumentClassification;}
+    public void setInstrumentClassification(String instrumentClassification) {
+        this.instrumentClassification = instrumentClassification;
+    }
 
 }
