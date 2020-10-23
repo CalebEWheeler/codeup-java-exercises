3c929a5a38f8e1f8ebd838f482782cca21a5a729
Caleb Wheeler <caleb.edward.wheeler@gmail.com>
2020-10-22 19:23:10 -0500

    feat: create constructor for Tablet that defines 3 parameters inherited from Device.class and a boolean indicating if the device has a keyboard

--- src/javaAssessment1/studyGuide/Tablet.java
+++ src/javaAssessment1/studyGuide/Tablet.java
@@ -3,5 +3,8 @@ package javaAssessment1.studyGuide;
 public class Tablet extends java_II_Device{
     protected boolean physicalKeyboard;
 
-
+    public Tablet(String type, String brand, double price, boolean physicalKeyboard) {
+        super(type, brand, price);
+        this.physicalKeyboard = physicalKeyboard;
+    }
 }
